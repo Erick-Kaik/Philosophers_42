9e818e2f92e4378a4082b8d0615456bc425bc551
Erick-Kaik <erick.kaik2002@gmail.com>
2023-06-28 14:22:52 -0300

    create header and modify 'anotações.txt'

---
+++ philo/src/philosophers.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekaik-ne <ekaik-ne@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/28 08:45:15 by ekaik-ne          #+#    #+#             */
+/*   Updated: 2023/06/28 08:59:34 by ekaik-ne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <pthread.h>
+
+#endif
